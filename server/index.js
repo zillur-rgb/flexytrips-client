bf4d5de8c7a4bf0326ba37ae841619b5fabdd0d1
zillur-rgb <zillurdeu@gmail.com>
2022-04-21 04:48:41 +0200

    Backend Ops for feedback is added

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const toursRouter = require("./controllers/tours");
+const feedbackRouter = require("./controllers/feedback");
 const dotenv = require("dotenv").config();
 const Tour = require("./model/tour");
 const cors = require("cors");
@@ -7,6 +8,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use("/api/tours", toursRouter);
+app.use("/api/feedbacks", feedbackRouter);
 
 //Calling PORT
 const PORT = process.env.PORT;
