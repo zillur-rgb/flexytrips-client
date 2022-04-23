a3cf3213285a6dfff61c08d33f4d1e423e5b9f93
zillur-rgb <zillurdeu@gmail.com>
2022-04-23 15:44:16 +0200

    Reload of Require problem solved

--- client/src/Components/RequireAuth/RequireAuth.jsx
+++ client/src/Components/RequireAuth/RequireAuth.jsx
@@ -3,8 +3,11 @@ import { Navigate, useLocation } from "react-router-dom";
 import { auth } from "../../firebase.init";
 
 const RequireAuth = ({ children }) => {
-  const [user] = useAuthState(auth);
+  const [user, loading] = useAuthState(auth);
   const location = useLocation();
+  if (loading) {
+    return <div>LOADING... PLEASE WAIT</div>;
+  }
   if (!user) {
     return <Navigate to="/login" state={{ from: location }} replace />;
   }
--- client/src/Pages/BookingSuccess/BookingSuccess.jsx
+++ client/src/Pages/BookingSuccess/BookingSuccess.jsx
@@ -1,10 +1,33 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import Navbar from "../../Components/Navbar/Navbar";
 
 const BookingSuccess = () => {
   return (
     <div>
-      <h1 className="header">Thanks for the Booking</h1>
-      <h1>See you at the tour day</h1>
+      <Navbar label="Sign In" />
+      <div
+        className="success-container"
+        style={{
+          margin: "100px 15%",
+        }}
+      >
+        <h1 className="header">Thanks for the Booking</h1>
+        <h1>See you at the tour day</h1>
+        <Link to="/">
+          <p
+            style={{
+              width: "150px",
+              marginTop: "20px",
+              borderBottom: "3px solid #333",
+              color: "#333",
+              paddingBottom: "10px",
+            }}
+          >
+            Go Back to Home
+          </p>
+        </Link>
+      </div>
     </div>
   );
 };
